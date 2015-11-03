e4ebc3dc574bd60abead6f3ea7084992d55f5cc7
ipinyol <ipinyol@gmail.com>
2015-11-03 17:25:18 -0500

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Set the proxy to enable http connections from the VM
+# Only enable the line if inside BCH, or replace proxy properly
+export http_proxy=http://proxy.tch.harvard.edu:3128
+
+# Install basic tools
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get -y install unzip
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo npm install express-generator -g
+sudo npm install express
+sudo apt-get -y install nodejs-legacy
